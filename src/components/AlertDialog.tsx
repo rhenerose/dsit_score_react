2ceb063d755bf5bc80254de7c22f51b2e3df10c8
rhenerose <kenroseryu@gmail.com>
2021-09-22 16:15:39 +0900

    add AlertDialog

---
+++ src/components/AlertDialog.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+const AlertDialog: React.FunctionComponent<{ title: any,msg: any, isOpen: any, doButton1: any }> = ({ title, msg, isOpen, doButton1 }) => {
+    const [open, setOpen] = React.useState(false);
+
+    // const handleClickOpen = () => {
+    //     setOpen(true);
+    // };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    React.useEffect(() => {
+        setOpen(isOpen)
+    }, [isOpen])
+
+    return (
+        <>
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    {title}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        {msg}
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => doButton1()} autoFocus>
+                        OK
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </>
+    );
+}
+
+export default AlertDialog;
